b0e587fdbfc7d2249dba6413e54075ec99ea310e
enkhriiileeenn <99162318+enkhriiileeenn@users.noreply.github.com>
2022-03-23 13:03:44 +0800

    Create movieticket.py

---
+++ movieticket.py
@@ -0,0 +1,8 @@
+"""
+'Welcome to Em's Movie Cinema'
+"""
+'Tuple Movie Choices'
+('Business Propasal';'Twenty One Twetny Five'; 'Thirty Nine';'Flower of Evil')
+row1= [1,2,3,4,5]
+row2 =[6,7,8,9,10]
+row3 =[11,12,13,14,15]
